251b1a7e700f543c06bf39c5684d095425d266df
Andrei Bahdanovich <bahdanovich@gmail.com>
2013-07-04 01:19:43 +0200

    all services script

---
+++ all-services.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+case "${1:-''}" in
+
+        'start')
+                sh xvfb.sh start
+                sh x11vnc.sh start
+                sh selenium-hub.sh start
+                sh selenium-node.sh start
+        ;;
+
+        'stop')
+                sh selenium-node.sh stop
+                sh selenium-hub.sh stop
+                sh x11vnc.sh stop
+                sh xvfb.sh stop
+        ;;
+
+        *)      # no parameter specified
+                echo "Usage: $0 start|stop"
+                exit 1
+        ;;
+
+esac
